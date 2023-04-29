d55a1b50239f088ee9eb278ac41d4530642c4c2b
Maksim Solovyev <solo@iMac-Maksim.local>
2023-04-29 13:33:39 +0500

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
